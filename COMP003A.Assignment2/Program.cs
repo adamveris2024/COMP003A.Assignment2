521de07fe651b50407060232b0dbd6b4213e9f05
adamveris2024 <adamveris2024@gmail.com>
2025-01-22 21:41:38 -0800

    Added comments and changed wording on some lines

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -1,36 +1,43 @@
-﻿using System.Globalization;
+﻿// Author: Adam Verissimo
+// Course: COMP003A
+// Instructor: Jonathan Cruz
+// Purpose: Discount Calculator
+using System.Globalization;
 
 namespace COMP003A.Assignment2
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   // these are the variables being set to what I need them to do
             string firstName;
             string lastName;
             int userAge;
             double itemPrice;
             bool isStudent;
 
+            // this is the top of the screen
             Console.WriteLine("********************************************************");
             Console.WriteLine("Welcome to Discount Calculator!");
             Console.WriteLine("********************************************************\n");
 
-            Console.Write("Enter your first name: ");
+            // These are the Console.Write for the user to input their information
+            Console.Write("What is your first name? ");
             firstName = Console.ReadLine();
 
-            Console.Write("Enter your last name: ");
+            Console.Write("What is your last name? ");
             lastName = Console.ReadLine();
 
-            Console.Write("What is your age: ");
+            Console.Write("How old are you? ");
             userAge = int.Parse(Console.ReadLine());
 
-            Console.Write("What is the price of the item you're interested in: ");
+            Console.Write("What is the price of the item you're interested in? ");
             itemPrice= double.Parse(Console.ReadLine());
 
             Console.Write("Are you a student? (true/false): ");
             isStudent = bool.Parse(Console.ReadLine());
 
+            // These are adding 2 lines to space out everything
             Console.WriteLine("\n");
             Console.WriteLine("\n");
 
@@ -40,6 +47,7 @@ namespace COMP003A.Assignment2
             double srPrice = itemPrice * 0.8;
 
 
+            // this is taking the information the user inputed and outputing it
             Console.WriteLine("********************************************************");
             Console.WriteLine($"Hello, " + firstName + " " + lastName + "!");
             Console.WriteLine($"Your current age is " + userAge + ".");
